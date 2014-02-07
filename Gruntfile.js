e58737912931f279d8be5618dc7e87d1fff8b773
Trey Shugart <treshugart@gmail.com>
2014-02-07 21:29:01 +1100

    Add travis file and deps for testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,52 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib');
-  grunt.config({
 
+  'use strict';
+
+  var host = grunt.option('host') || 'localhost';
+
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.initConfig({
+    concat: {
+      all: {
+        files: {
+          'dist/skate.js': [
+            'src/skate.js'
+          ]
+        }
+      }
+    },
+    karma: {
+      all: {
+        options: {
+          browsers: ['PhantomJS'],
+          files: [
+            'dist/skate.js',
+            'tests/skate.js'
+          ],
+          frameworks: ['mocha', 'sinon-chai'],
+          singleRun: true
+        }
+      }
+    },
+    uglify: {
+      all: {
+        files: {
+          'dist/skate.min.js': 'dist/skate.js'
+        }
+      }
+    },
+    watch: {
+      test: {
+        files: ['src/*.js'],
+        tasks: ['dist']
+      }
+    }
   });
+
+  grunt.registerTask('build', 'Runs the tests and builds the dist.', ['test']);
+  grunt.registerTask('dist', 'Builds the dist.', ['concat', 'uglify']);
+  grunt.registerTask('test', 'Runs the tests.', ['dist', 'karma']);
+
 };
